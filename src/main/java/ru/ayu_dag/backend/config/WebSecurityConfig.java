cf14c787ac1d9977071abdce9e9cf27068beb9b2
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-12-31 17:45:21 +0300

    refactoring

--- src/main/java/ru/ayu_dag/backend/config/WebSecurityConfig.java
+++ src/main/java/ru/ayu_dag/backend/config/WebSecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.ayu_dag.backend.jwt.JWTAuthorizationFilter;
