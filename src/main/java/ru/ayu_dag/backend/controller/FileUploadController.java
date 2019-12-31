ebb8118be6ff14d4e44fadbc426550452a5f2df2
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-12-31 15:19:25 +0300

    add hot reload

--- src/main/java/ru/ayu_dag/backend/controller/FileUploadController.java
+++ src/main/java/ru/ayu_dag/backend/controller/FileUploadController.java
@@ -1,5 +1,7 @@
 package ru.ayu_dag.backend.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,9 +13,11 @@ import java.io.IOException;
 public class FileUploadController {
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public void UploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<String> UploadFile(@RequestParam("file") MultipartFile file) throws IOException {
         System.out.println(file.getName());
         System.out.println(file.getBytes().length);
         System.out.println(file.getOriginalFilename());
+        System.out.println(file.isEmpty());
+        return new ResponseEntity<>("Load File with Name" + file.getOriginalFilename(), HttpStatus.OK);
     }
 }
